ff4c9c6434858c78bae6bd0e5ca233669f069191
seanivanfabia@gmail.com <99702949+lirrnaiad@users.noreply.github.com>
2024-12-18 15:19:59 +0800

    Fraction input fix

--- simplex.py
+++ simplex.py
@@ -65,27 +65,13 @@ def convert_term(term: str):
     elif term == "-":
         term = "-1"
 
-    if "." in term:     # Convert to float if there is a decimal point
+    if "/" in term:
+        numerator, denominator = term.split("/")
+        return float(numerator) / float(denominator)
+    elif "." in term:     # Convert to float if there is a decimal point
         return float(term)
-    elif "/" in term:
-        # If the term is a fraction use the helper function convert_term_to_fraction()
-        term = convert_term_to_fraction(term)
-        return term
     else:
         return int(term)
-
-
-# If the term is a fraction, this function will be used
-def convert_term_to_fraction(term: str) -> fractions.Fraction:
-    # Remove x or y first
-    if "x" in term or "y" in term:
-        term = term.replace("x", "").replace("y", "")
-
-    numerator, denominator = term.split("/")
-
-    numerator = int(numerator)
-    denominator = int(denominator)
-    return fractions.Fraction(numerator, denominator)
 
 
 def get_objective_function() -> str:
